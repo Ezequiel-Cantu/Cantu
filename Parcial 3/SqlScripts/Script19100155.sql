ad00130649d7408a51f55d499f3ea20465a06af0
Ezequiel-Cantu <70524684+Ezequiel-Cantu@users.noreply.github.com>
2022-05-23 14:45:41 -0500

    SQL

---
+++ Parcial 3/SqlScripts/Script19100155.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE 19100155;
+USE 19100155;
+
+CREATE TABLE Usuario
+(
+    idUsuario SMALLINT NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR(30),
+    ApPat VARCHAR(30),
+    Edad INT(2),
+    Ocupacion VARCHAR(30),
+    Usuario VARCHAR(30),
+    Telefono BIGINT(10),
+    Genero VARCHAR(20),
+    Contrasena VARCHAR(30),
+    PRIMARY KEY(idUsuario)
+);
+
+INSERT INTO Usuario (Nombre,ApPat,Edad,Ocupacion,Usuario, Telefono, Genero, Contrasena)
+VALUES ('Ezequiel', 'Cantu', '21', 'Programador', 'L19100155', '8671829812', 'Masculino', '148j3shi');
