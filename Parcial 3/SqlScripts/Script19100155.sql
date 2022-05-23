7eebfdf031e12e0b909b7f0166ebfc86a18b83df
Ezequiel-Cantu <70524684+Ezequiel-Cantu@users.noreply.github.com>
2022-05-23 15:16:56 -0500

    SQL

--- Parcial 3/SqlScripts/Script19100155.sql
+++ Parcial 3/SqlScripts/Script19100155.sql
@@ -1,18 +1,17 @@
-CREATE DATABASE 19100155;
-USE 19100155;
+CREATE DATABASE IF NOT EXISTS L19100155;
 
-CREATE TABLE Usuario
+CREATE TABLE IF NOT EXISTS Usuario
 (
     idUsuario SMALLINT NOT NULL AUTO_INCREMENT,
-    Nombre VARCHAR(30),
-    ApPat VARCHAR(30),
-    Edad INT(2),
-    Ocupacion VARCHAR(30),
-    Usuario VARCHAR(30),
-    Telefono BIGINT(10),
-    Genero VARCHAR(20),
-    Contrasena VARCHAR(30),
-    PRIMARY KEY(idUsuario)
+    Nombre VARCHAR (30),
+    ApPat VARCHAR (30),
+    Edad INT (2),
+    Ocupacion VARCHAR (30),
+    Usuario VARCHAR (30),
+    Telefono BIGINT (10),
+    Genero VARCHAR (20),
+    Contrasena VARCHAR (30),
+    PRIMARY KEY (idUsuario)
 );
 
 INSERT INTO Usuario (Nombre,ApPat,Edad,Ocupacion,Usuario, Telefono, Genero, Contrasena)
