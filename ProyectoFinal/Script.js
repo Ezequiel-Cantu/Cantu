8fe380a087bfb615b74c4ae8bc82255cf1440dda
Ezequiel-Cantu <70524684+Ezequiel-Cantu@users.noreply.github.com>
2022-06-01 12:06:02 -0500

    Eliminar

--- ProyectoFinal/Script.js
+++ ProyectoFinal/Script.js
@@ -1,36 +1,34 @@
 $(document).ready(function () {
     $('#btnModifBD'). click(function()
     {
-        try{
-            let parid = prompt("Teclee el ID a modificar");
-            let nom = document.getElementById("Nombre").value;
-            let Ape = document.getElementById("Apellido").value;
-            let Edad = document.getElementById("Edad").value;
-            let Ocupacion = document.getElementById("Ocupacion").value;
-            let Telefono = document.getElementById("telefono").value;
-            let Usernam = document.getElementById("username").value;
-            let Pass = document.getElementById("password").value;
-            let gener = document.getElementById("gender").value;
+        let nom = document.getElementById("Nombre").value;
+        let Ape = document.getElementById("Apellido").value;
+        let Edad = document.getElementById("Edad").value;
+        let Ocupacion = document.getElementById("Ocupacion").value;
+        let Telefono = document.getElementById("telefono").value;
+        let Usernam = document.getElementById("username").value;
+        let Pass = document.getElementById("password").value;
+        let gener = document.getElementById("gender").value;
 
-            $.post('ArchivoPHP/getRegistroBD.php', {par1:parid}, function(data){
-                    if(data.idUsuario == null){
-                        swal("Error","No se encontro el registro", "error");
-                    }
-                    else{
-                        $.post('ArchivoPHP/modRegistro.php', {par1:parid, par2:nom, par3:Ape, par4:Edad, par5:Ocupacion, par6:Telefono, par7:Usernam, par8:Pass, par9:gener},
-                        function(data){
-                        }, 'json');
-                        swal("Accion completada", "Se ha modificado correctamente", "success");
-                        LimpiarFormulario();
-                    }
-                },'json');
-
-        }
-        catch (exception){
-            swal("Error", "No se ha concretado la accion", "error");
-        }
-    });
-
+        if(nom == "" || Ape == "" || Edad == "" || Ocupacion == "" || Telefono == "" || Usernam == "" || Pass == "" || gener == ""){
+            swal("Teclee el ID a Modificar: ",{
+            content: "input",
+        })
+        .then((parid) => {
+                if(data.idUsuario == null){
+                    swal("Error","No se encontro el registro", "error");
+                }
+                else{
+                    $.post('ArchivoPHP/modRegistro.php', {par1:parid, par2:nom, par3:Ape, par4:Edad, par5:Ocupacion, par6:Telefono, par7:Usernam, par8:Pass, par9:gener},
+                    function(data){
+                    }, 'json');
+                    swal("Accion completada", "Se ha modificado correctamente", "success");
+                    LimpiarFormulario();
+                }
+            },'json');
+        })
+    }
+});
     $('#btnAgregar').click(function(){
 
         try{
@@ -60,32 +58,47 @@ $(document).ready(function () {
     });
 
     $('#btnElimBD').click(function(){
-        let parid = prompt("Teclee el ID a eliminar");
+        
+        
+            var nom = document.getElementById("Nombre").value;
+            var Ape = document.getElementById("Apellido").value;
+            var Edad = document.getElementById("Edad").value;
+            var Ocupacion = document.getElementById("Ocupacion").value;
+            var Telefono = document.getElementById("telefono").value;
+            var Usernam = document.getElementById("username").value;
+            var Pass = document.getElementById("password").value;
+            var gener = document.getElementById("gender").value;
 
-        if(parid == ""){
-            swal("Error","No deje el campo vacio", "error"); 
+            if(nom == "" || Ape == "" || Edad == "" || Ocupacion == "" || Telefono == "" || Usernam == "" || Pass == "" || gener == ""){
+                swal("Teclee el ID a eliminar: ",{
+                content: "input",
+            })
+            .then((parid) => {
+                $.post('ArchivoPHP/getRegistroBD.php', {par1:parid}, function(data){
+                    if(data.idUsuario == null){
+                        swal("Error","No se encontro el registro", "error"); 
+                    }
+                    else{
+                        $.post('ArchivoPHP/ElimRegistro.php', {par1:parid},function(data){
+                        },'json');
+                        swal("Accion completada", "Se ha eliminado correctamente", "success");
+                            LimpiarFormulario();
+                    }
+                },'json');
+            })
         }
-        else {
-            $.post('ArchivoPHP/getRegistroBD.php', {par1:parid}, function(data){
-                if(data.idUsuario == null){
-                    swal("Error","No se encontro el registro", "error"); 
-                }
-                else{
-                    $.post('ArchivoPHP/ElimRegistro.php', {par1:parid},function(data){
-                    },'json');
-                    swal("Accion completada", "Se ha eliminado correctamente", "success");
-                        LimpiarFormulario();
-                }
-            },'json');
-        }
+         
     });
 
     $('#btnConsBD').click(function() {
-        let parid=prompt("Teclee el ID a consultar");
-
-        $.post('ArchivoPHP/getRegistroBD.php',{par1:parid},function(data){
-          refrescar(data);
-          },'json');
+        swal("Teclee el ID a consultar: ",{
+            content: "input",
+        })
+        .then((parid) => {
+            $.post('ArchivoPHP/getRegistroBD.php',{par1:parid},function(data){
+                refrescar(data);
+                },'json');
+        })
     });
     function refrescar(objeto) {
         console.log(objeto);
@@ -110,4 +123,3 @@ $(document).ready(function () {
         $('#gender').val("");
     }
 });
-
